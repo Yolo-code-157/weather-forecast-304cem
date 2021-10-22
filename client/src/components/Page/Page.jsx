20d091435f8a2391a1d20ca2828d51af531b3fee
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-10-22 16:23:55 +0800

    something

--- client/src/components/Page/Page.jsx
+++ client/src/components/Page/Page.jsx
@@ -44,7 +44,7 @@ const Page = () => {
         .catch((err) => {
             console.log(err);
         });
-    },[])
+    })
     
     const { isError, isLoading, forecast, submitRequest } = useForecast();
 
@@ -86,7 +86,7 @@ const Page = () => {
                         
                         <div className={`${styles.box1} position-relative`}>
                             <div>
-                                <img src={`https://openweathermap.org/img/wn/${weather.wheaIcon}@2x.png`}alt="imgicon"/>
+                                {/* <img src={`https://openweathermap.org/img/wn/${weather.wheaIcon}@2x.png`}alt="imgicon"/> */}
                                 <h1>{weather.localTitle}, {weather.wheaCount}</h1>
                                 <p>Type: {weather.localType}</p>
                                 <p>Coord: <br/>{weather.localLatLon}</p> 
