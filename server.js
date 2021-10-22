a01f8e59bfe279b209f0221ee536dfaf2c62cc4b
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-10-22 18:22:03 +0800

    csjdc

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
 
 //where login and register function
 app.use('/api/users', userRoutes);
@@ -101,7 +102,7 @@ app.delete('/delete/:id', (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 5000;
+
 
 //Port listen
 app.listen(PORT, ()=>{
